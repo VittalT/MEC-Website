dad883fef302813cbee71f6a16293f99fb882708
Vittal Thirumalai <vittalt@mit.edu>
2023-10-24 00:52:40 -0400

    Added home page and nav bar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function redirectToSignupPage() {
+  window.location.href = "signup.html";
+}
+
+function redirectToHomePage() {
+  window.location.href = "index.html";
+}
+
+function redirectToTeamPage() {
+  window.location.href = "team.html";
+}
+
+function redirectToStartupInterfacePage() {
+  window.location.href = "startup-interface.html";
+}
+
+function redirectToStarthousePage() {
+  window.location.href = "starthouse.html";
+}
+
+function redirectToContactUsPage() {
+  window.location.href = "contact.html";
+}
